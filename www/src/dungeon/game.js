fe616e7c7562714454a49bb133eced0f23acab8a
miraku-5040 <20203061-onoderaseiki@hcs.ac.jp>
2022-12-01 11:18:47 +0900

    画像とステージの形成

---
+++ www/src/dungeon/game.js
@@ -0,0 +1,12 @@
+// 起動されたときに呼ばれる関数を登録する
+window.addEventListener("load", () => {
+    // まずステージを整える
+    initialize();
+});
+
+function initialize() {
+    // 画像を準備する
+    Image.initialize();
+    // ステージを準備する
+    Stage.initialize();
+}
--- www/src/dungeon/image.js
+++ www/src/dungeon/image.js
@@ -1,2 +1,33 @@
-// This is a JavaScript file
+class Image {
+
+    static initialize() {
+        //stageImages 床　無　壁上右下左　角　道角
+        this.stageImages = [];
+        for(let i=0; i<5; i++){
+            const image = document.getElementById(`stage${i+1}`);
+            image.removeAttribute('id');
+            image.width = Config.stageImgWidth;
+            image.height = Config.stageImgHeight;
+            image.style.position = 'absolute';
+            this.stageImages.push(image);
+            if(i>1){
+            const newImage = image.cloneNode(false);
+            newImage.style.transform = "rotate(90deg)"
+            this.stageImages.push(newImage);
+
+            const newImage1 = image.cloneNode(false);
+            newImage1.style.transform = "rotate(180deg)"
+            this.stageImages.push(newImage1);
+
+            const newImage2 = image.cloneNode(false);
+            newImage2.style.transform = "rotate(270deg)"
+            this.stageImages.push(newImage2);
+            }
+           
+        }
+    }
+
+    //getStageImage
+    //メソッドを定義
+}
 
