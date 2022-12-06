b518ec326d667c09037384cc7e5ae5ae273c1f34
yume-01 <20203006-sukegawayume@hcs.ac.jp>
2022-12-06 14:38:18 +0900

    音声追加

--- www/js/page.js
+++ www/js/page.js
@@ -1,49 +1,79 @@
 function toMypage() {
+    // 音声再生
+    go_sound();
     // マイページ画面へ遷移
-    window.location.href = "mypage.html";
+    setTimeout('window.location.href = "mypage.html"', 600);
 }
 
 function toSelectDanjyon() {
+    // 音声再生
+    go_sound();
     // ダンジョン選択画面へ遷移
-    window.location.href = "../index.html";
+    setTimeout('window.location.href = "../index.html"', 600);
+}
+
+function toGameImage() {
+    // 音声再生
+    audio = new Audio("../sound/start.m4a");
+    audio.play();
+    // スタート画面からのダンジョン選択画面へ遷移
+    setTimeout('window.location.href = "../index.html"', 600);
 }
 
 function toGatya() {
+    // 音声再生
+    go_sound();
     // ガチャ画面へ遷移
-    window.location.href = "gatya.html";
+    setTimeout('window.location.href = "gatya.html"', 600);
 }
 
 function resultOne() {
+    // 音声再生
+    audio = new Audio("../sound/gatya.m4a");
+    audio.play();
     // 1回ガチャ結果画面へ遷移
-    window.location.href = "resultOne.html";
+    setTimeout('window.location.href = "resultOne.html"', 1200);
 }
 
 function resultTen() {
+    // 音声再生
+    audio = new Audio("../sound/gatya.m4a");
+    audio.play();
     // 10回ガチャ結果画面へ遷移
-    window.location.href = "resultTen.html";
+    setTimeout('window.location.href = "resultTen.html"', 1200);
 }
 
 function toItem() {
-    // アイテム画面へ遷移
-    window.location.href = "item.html";
+    // 音声再生
+    go_sound();
+    // 持ち物画面へ遷移
+    setTimeout('window.location.href = "item.html"', 600);
 }
 
 function toPower() {
+    // 音声再生
+    go_sound();
     // 強化画面へ遷移
-    window.location.href = "power.html";
+    setTimeout('window.location.href = "power.html"', 600);
 }
 
 function toSet() {
+    // 音声再生
+    go_sound();
     // 設定画面へ遷移
-    window.location.href = "setting.html";
+    setTimeout('window.location.href = "setting.html"', 600);
 }
 
 function toStart() {
-    // ダンジョン画面へ遷移
-    window.location.href = "start.html";
+    // 音声再生
+    go_sound();
+    // スタート画面へ遷移
+    setTimeout('window.location.href = "start.html"', 600);
 }
 
-function toDanjyon() {
-    // ダンジョン画面へ遷移
-    window.location.href = "danjyon.html";
+// 音声再生用のjs
+// 画面遷移用の音声再生
+function go_sound() {
+    audio = new Audio("../sound/select.m4a");
+    audio.play();
 }
