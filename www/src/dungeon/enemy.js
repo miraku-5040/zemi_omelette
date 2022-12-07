ac050663bc00b97d964a956dc755215edd976d2d
sato-5555 <20203039-satohiroki@hcs.ac.jp>
2022-12-07 13:43:30 +0900

    enemy.js作成途中

--- www/src/dungeon/enemy.js
+++ www/src/dungeon/enemy.js
@@ -9,7 +9,7 @@ class Enemy{
 
         /* 開発用にデータをセットする */
         //x=7, y=7に敵をセット
-        this.enemyStatusArray = [{"enemyId":"enemy_1", "enemyName":"スライム", "hp":{"current":100, "max":100, "min":0}, "currentX":7, "currentY":7, "size":1}];
+        this.enemyStatusArray = [{enemyId:1, enemyName:"スライム", hp:{current:100, max:100, min:0}, currentX:7, currentY:7, size:1}];
         /* end */
 
 
@@ -27,14 +27,15 @@ class Enemy{
     }
     static screenRenderingOne(enemyId = null) {
         const matchArray = this.enemyStatusArray.filter((array) => array.enemyId === enemyId);
-        if(matchArray.length() > 0){
-            matchArray.forEach(element => this.#screenRenderingExe(element));
-        }else{
-            //enemyIdが存在しない場合
+        if(matchArray.length() !== 1){
+            //件数が1件以外 NG
+            return false;
         }
+        matchArray.forEach(element => this.#screenRenderingExe(element));
     }
     static #screenRenderingExe(enemyStatusArrayElement){
         const enemyLayerElement = document.getElementById("enemy_layer");
+        const imgElement = document.createElement('img');
 
     }
 
