40272dd0b9c577429b58e26d426949cd2a82f7f4
yume-01 <20203006-sukegawayume@hcs.ac.jp>
2022-12-19 11:49:12 +0900

    CSS変更

--- www/js/db/danjyon_db.js
+++ www/js/db/danjyon_db.js
@@ -6,21 +6,12 @@ var DANJYON_DB = "danjyon";
 var ITEM_DB = "item";
 var WEAPON_DB = "weapon";
 
-// 処理を一時停止する
-const wait = (sec) => { // タイマ
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, sec * 1000);
-        //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
-    });
-};
-
 // ダンジョン情報全件取得
 function getDanjyonData() {
     var ncmb = new NCMB(this.APPLICATION_KEY, this.CLIENT_KEY);
     var Danjyon = ncmb.DataStore(this.DANJYON_DB);
     Danjyon.fetchAll()
         .then(function (results) {
-            console.log(results);
             setDanjyonName(results);
         })
         .catch(function (err) {
@@ -29,38 +20,14 @@ function getDanjyonData() {
 
     // ダンジョン名をHTMLに埋め込む
     function setDanjyonName(results) {
+        // 初期化 document.getElementById("note").innerHTML = '';
         // 情報取得
         for (var i = 0; i <= results.length - 1; i++) {
             var danjyon = results[i];
             // 新しいHTML要素を作成
-            var danjyonHtml = '<div><img class="note_button_image" src="../image/danjyonSelectButton.png" onclick="modalOpen()"><p class="note_button_text">' + danjyon.danjyon_name + '</p></div>';
+            var danjyonHtml = '<div><img class="note_button_image" id="' + danjyon.danjyon_id + '" src="../image/danjyonSelectButton.png" onclick="modalOpen();searchDanjyonData(this)" value="' + danjyon.danjyon_id + '"><p class="note_button_text">' + danjyon.danjyon_name + '</p></div>';
             // 作成した要素を追加
             document.getElementById("note").insertAdjacentHTML('beforeend', danjyonHtml);
-        }
-    }
-}
-
-// ダンジョン情報詳細取得
-function searchDanjyonData(danjyonName) {
-    var ncmb = new NCMB(this.APPLICATION_KEY, this.CLIENT_KEY);
-    var Danjyon = ncmb.DataStore(this.DANJYON_DB);
-    Danjyon.equalTo("danjyon_name", danjyonName)
-        .fetchAll()
-        .then(function (results) {
-            setDanjyonDetail(results);
-        })
-        .catch(function (err) {
-            console.log("ng" + err);
-        });
-
-    // ダンジョンの説明をHTMLに埋め込む
-    function setDanjyonDetail(results) {
-        // 情報取得
-        // 新しいHTML要素を作成
-        var danjyonHtml = '<p class="modal_title" >' + results.danjyon_name + '</p><p class="modal_note">' + results.danjyon_detail + '</p>';
-        // 作成した要素を追加
-        window.onload = function () {
-            document.getElementById("modal_text").insertAdjacentHTML('beforeend', danjyonHtml)
         }
     }
 }
