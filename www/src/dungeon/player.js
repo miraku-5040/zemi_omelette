4ea83bfffa3157d189f14606751cdc4e52633886
miraku-5040 <20203061-onoderaseiki@hcs.ac.jp>
2023-02-08 14:28:16 +0900

    攻撃時の防御力の受け渡しできてない

--- www/src/dungeon/player.js
+++ www/src/dungeon/player.js
@@ -323,8 +323,8 @@ class Player{
         }
         const matchPlayerStatus = this.playerStatus;
         const playerDefenseStatus = {};
-        playerAttackStatus.def = this.checkLimitValue(matchPlayerStatus,'def')
-        playerAttackStatus.avd = this.checkLimitValue(matchPlayerStatus,'avd')
+        playerDefenseStatus.def = this.checkLimitValue(matchPlayerStatus,'def')
+        playerDefenseStatus.avd = this.checkLimitValue(matchPlayerStatus,'avd')
 
         return playerDefenseStatus;
     }
